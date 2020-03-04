27dd21c629e069adc96f139d928516201f090719
shimizuyuya <blanc2756@gmail.com>
2020-03-05 06:47:22 +0900

    a

--- work_io/lib/widgets/workAggregation_widget.dart
+++ work_io/lib/widgets/workAggregation_widget.dart
@@ -8,36 +8,40 @@ class ChangeForm extends StatefulWidget {
 class _ChangeFormState extends State<ChangeForm> {
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.tealAccent[400],
-      padding: const EdgeInsets.all(6),
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            // Text(
-            //   "$_text",
-            //   style: TextStyle(
-            //       color: Colors.blue,
-            //       fontSize: 30.0,
-            //       fontWeight: FontWeight.w500),
-            // ),
-            TextField(
-              enabled: true,
-              // 入力数
-              // maxLength: 10,
-              maxLengthEnforced: false,
-              style: TextStyle(color: Colors.red),
-              // obscureText: false,
-              maxLines: 1,
-              //パスワード
-              obscureText: true,
-              decoration: InputDecoration(
-                focusColor: Colors.grey,
-                border: OutlineInputBorder(),
-                labelText: '打刻メモ',
-              ),
+      margin: const EdgeInsets.only(
+        left: 30.0,
+        top: 10.0,
+        right: 30.0,
+        bottom: 20,
+      ),
+      height: 20.0,
+      decoration: new BoxDecoration(
+        color: Colors.white,
+        borderRadius: new BorderRadius.all(new Radius.circular(5)),
+      ),
+      // child: SingleChildScrollView(
+      child: new Directionality(
+        textDirection: TextDirection.ltr,
+        child: new TextField(
+          controller: null,
+          autofocus: false,
+          style: new TextStyle(fontSize: 15.0, color: Color(0xFFbdc6cf)),
+          decoration: new InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: '打刻メモ',
+            contentPadding: const EdgeInsets.only(left: 14.0, bottom: 10),
+            focusedBorder: OutlineInputBorder(
+              borderSide: new BorderSide(color: Colors.white),
+              borderRadius: new BorderRadius.circular(5),
             ),
-          ],
+            enabledBorder: UnderlineInputBorder(
+              borderSide: new BorderSide(color: Colors.white),
+              borderRadius: new BorderRadius.circular(5),
+            ),
+          ),
         ),
+        // ),
       ),
     );
   }
